7c93807ecdb097c07f4e01ce3c7f98c4180d922b
NotImplementedLife <not.implemented.life@gmail.com>
2021-07-01 20:16:43 +0300

    intro [sketch]

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,11 @@
 INCLUDE "src/include/hardware.inc"
 INCLUDE "src/include/macros.inc"
 
+SECTION "Vars", WRAM0
+
+X:
+DS 1
+
 SECTION "Main", ROM0
 
 MACRO loadVRAM
@@ -10,24 +15,80 @@ MACRO loadVRAM
 	call loadMemorySTAT
 ENDM
 
+
+
 Start::
+	ld a, $0A
+	ld [$0000], a
+	;ld a, $01
+	;ld [$A000], a
+	xor a
+	ld [X], a
 	call waitForVBlank
 	ld a, LCDCF_ON | LCDCF_BGON
 	ld [rLCDC], a
 	ld a, %11100100
 	ld [rBGP], a
+	;ld [X], a
+	
+	ld a, 48
+	ld [rSCX], a	
+	
 	xor a
 	ld [wJoypadState], a
 	ld [wJoypadPressed], a
 	
 	call waitForVBlank
 	
-	loadVRAM $8800, Tiles, TilesEnd
-	loadVRAM $9800, TM, TME
+	;loadVRAM $8800, Tiles, TilesEnd
+	;loadVRAM $9800, TM, TME
 	
-.loop
-	call updateJoypadState
+	loadVRAM $9000, Actual_TS, Actual_TS + $800
+	loadVRAM $8800, Actual_TS + $800, Actual_TS_End	
+	
+	loadVRAM $8000, Chars, Chars_End	
+	
+	loadVRAM $9800, Actual_TM, Actual_TM_End
+		
 	call waitForVBlank
+.loop	
+	.waitVRAM
+    ldh a, [rSTAT]
+    and STATF_BUSY ; %0000_0010
+    jr nz, .waitVRAM	
+	
+	;ldh a, [rBGP]		
+	;rlca		
+	;rlca	
+	;ldh [rBGP], a	
+	
+	ldh a, [rLY]
+	cp a, 94
+	jr nz, .skipLCDC	
+	
+	ldh a, [rLCDC]
+	or LCDCF_BG8000
+	ldh [rLCDC], a	
+.skipLCDC:
+	ldh a, [rLY]
+	cp a, 143
+	jr nz, .skipLCDCres
+	ldh a, [rLCDC]
+	and ($FF ^ LCDCF_BG8000)
+	ldh [rLCDC], a	
+.skipLCDCres:
+	jr .loop
+
+	ld a, %00011011
+	ldh [rBGP], a	
+	
+	;call waitForVBlank	
+	;call updateJoypadState	
+	
+	;xor a
+	;ld [X], a
+	
+	jr .loop
 	
 	ld a, [wJoypadState]
 	and PADF_UP	
